6ecdcf008d1660dd65a14bb5a3a87a972bdc6675
ErbolKarmyshakov <erbolkarmysakov37@gmail.com>
2023-10-26 13:17:25 -0500

    Initial Commit

---
+++ Month4-Homework2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Month4-Homework2
+//
+//  Created by Mac User on 26/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
